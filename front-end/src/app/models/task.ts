bb8ff2ec2d939d1c6a916e3eae55d89314003455
Kevin <devkevferreira@gmail.com>
2025-03-15 18:40:02 -0300

    feat(front-end): Implementa a interface da Task

---
+++ front-end/src/app/models/task.ts
@@ -0,0 +1,11 @@
+export interface Task {
+  id: number;
+  title: string;
+  description: string;
+  owner: string;
+  due_date: string;
+  created_at: string;
+  deleted_at?: string;
+  status: 'todo' | 'in-progress' | 'done' | 'pending';
+  completed: boolean;
+}
