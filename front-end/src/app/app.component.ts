13d6a57a58e564e37b310ad9e5b415bfde06c7b4
Kevin <devkevferreira@gmail.com>
2025-03-15 19:15:02 -0300

    feat(front-end): Configura o App component e adiciona o estilo global

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TaskBoardComponent } from './components/task-board/task-board.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    TaskBoardComponent,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'front-end';
-}
+export class AppComponent {}
