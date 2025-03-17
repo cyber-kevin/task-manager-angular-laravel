30d1bb2b9fd6f8ca75593801b1fdb68e993cf6fc
Kevin <devkevferreira@gmail.com>
2025-03-17 06:48:25 -0300

    fix(front-end): Adiciona token do usuario aos headers das requisicoes

--- front-end/src/app/services/api.service.ts
+++ front-end/src/app/services/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 
@@ -8,23 +8,27 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
   private apiUrl = environment.apiUrl;
+  private token = localStorage.getItem('access_token');
+  private headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`
+    });
 
   constructor(private http: HttpClient) { }
 
   getTasks(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/tasks`);
+    return this.http.get(`${this.apiUrl}/tasks`, { headers: this.headers });
   }
 
   createTask(task: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/tasks`, task);
+    return this.http.post(`${this.apiUrl}/tasks`, task, { headers: this.headers });
   }
 
   updateTask(id: number, task: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/tasks/${id}`, task);
+    return this.http.put(`${this.apiUrl}/tasks/${id}`, task, { headers: this.headers });
   }
 
   deleteTask(id: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/tasks/${id}`);
+    return this.http.delete(`${this.apiUrl}/tasks/${id}`, { headers: this.headers });
   }
 
   login(email: string, password: string): Observable<any> {
