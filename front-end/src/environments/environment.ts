d27c797e0dbce28df8bbcb447e58bed701f8b53a
Kevin <devkevferreira@gmail.com>
2025-03-17 09:14:49 -0300

    feat(front-end): Configura a URL da API Laravel

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8000/api'
+    production: true,
+    apiUrl: 'https://task-manager-backend-laravel-production.up.railway.app/api'
 };
